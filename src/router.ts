e2f473e3a4876fd4c99d8452c95e9de1f67a776a
misbah <misbah.qadri@gmail.com>
2022-03-17 10:03:02 +0700

    Menghapus custom webhook

--- src/router.ts
+++ src/router.ts
@@ -3,5 +3,5 @@ import { homeHandler } from "./api/home";
 import { webhookHandler } from "./api/webhook";
 export const router = (app: Polka) => {
   app.get("/", homeHandler);
-  app.post(`/webhook/${process.env.APP_RANDOM_PATH}`, webhookHandler);
+  app.post(`/webhook`, webhookHandler);
 };
